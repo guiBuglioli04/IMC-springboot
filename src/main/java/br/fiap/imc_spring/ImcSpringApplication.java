d729519673dc145cc9dc74990350dcd6b5e89764
Guilherme Buglioli <gui_buglioli@hotmail.com>
2025-10-07 11:16:21 -0300

    first commit

---
+++ src/main/java/br/fiap/imc_spring/ImcSpringApplication.java
@@ -0,0 +1,13 @@
+package br.fiap.imc_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImcSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImcSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/fiap/imc_spring/ImcSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package br.fiap.imc_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImcSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
